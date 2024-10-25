355b32c294b90f4e563714bfde369e4bf2e83493
Danny <varmarid@gmail.com>
2024-10-25 14:07:02 +0300

    created user schema mongoose

---
+++ server/models/User.ts
@@ -0,0 +1,16 @@
+import { model, Schema } from "mongoose";
+interface User {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<User>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = model<User>("User", userSchema);
+
+export default User;
