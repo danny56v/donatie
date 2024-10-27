5f92ef2f41e7aa694dde82e14bcdae93f786e671
Danny <varmarid@gmail.com>
2024-10-27 12:38:20 +0200

    signup api functionallity

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { json } from "body-parser";
 
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-import authRouter from './routes/auth.route'
+import authRouter from "./routes/auth.route";
 
 dotenv.config();
+
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World"); 
-// });
-
-
+app.use(json());
 
 main().catch((err) => console.log(err));
-  
-async function main() { 
+
+async function main() {
   await mongoose.connect(process.env.MONGO_URL);
-  console.log('DATABASE CONNECTED')
+  console.log("DATABASE CONNECTED");
 }
 
+app.use("/api/auth", authRouter);
 
-app.use('/api/auth', authRouter)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000, () => {
   console.log("App is listening on port 3000");
