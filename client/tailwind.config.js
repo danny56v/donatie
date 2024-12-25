929fc7c210225aaedb2a565f6f42872ed03ae044
Danny <varmarid@gmail.com>
2024-12-25 16:49:57 +0200

    new version on signIn page with tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    
+    },
   },
   plugins: [],
-}
+});
+
+// module.exports = withMT({
+//   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// });
