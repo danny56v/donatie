e7dc6bd228d3875e698baaf9a9fe51cd15778a87
Danny <varmarid@gmail.com>
2024-12-27 18:28:39 +0200

    make some changes for category

--- server/controllers/category.controller.ts
+++ server/controllers/category.controller.ts
@@ -1,6 +1,16 @@
 import { RequestHandler } from "express";
 import { Category } from "../models/Category";
 import { errorHandler } from "../utils/error";
+import { Subcategory } from "../models/Subcategory";
+
+export const getCategories: RequestHandler = async (req, res, next) => {
+  try {
+    const categories = await Category.find().populate("subcategories");
+    res.status(200).json(categories);
+  } catch (error) {
+    return next(errorHandler(500, "Eroare la găsirea categoriilor"));
+  }
+};
 
 export const addCategory: RequestHandler = async (req, res, next) => {
   try {
@@ -22,20 +32,17 @@ export const addCategory: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const changeCategoryName: RequestHandler = async (req, res, next) => {
+export const updateCategory: RequestHandler = async (req, res, next) => {
+  const categoryId = req.params.categoryId;
   try {
     if (!req.user || !req.user.id || !req.user.isAdmin) {
       return next(errorHandler(401, "Acces restricționat - necesită drepturi de administrator!"));
     }
-    const { oldName, newName } = req.body;
-    if (!oldName || !newName) {
+    const { name } = req.body;
+    if (!name) {
       return next(errorHandler(400, "Campurile sunt obligatorii!"));
     }
-    const category = await Category.findOneAndUpdate(
-      { name: oldName },
-      { name: newName },
-      { new: true, runValidators: true }
-    );
+    const category = await Category.findByIdAndUpdate(categoryId, { name }, { new: true, runValidators: true });
     if (!category) {
       return next(errorHandler(404, "Categoria nu a fost găsită."));
     }
@@ -48,19 +55,25 @@ export const changeCategoryName: RequestHandler = async (req, res, next) => {
 };
 
 export const deleteCategory: RequestHandler = async (req, res, next) => {
+  const categoryId = req.params.categoryId;
   try {
     if (!req.user || !req.user.id || !req.user.isAdmin) {
       return next(errorHandler(401, "Acces restricționat - necesită drepturi de administrator!"));
     }
-    const { name } = req.body;
 
-    const deletedCategory = await Category.findOneAndDelete({ name });
+    const category = await Category.findById(categoryId);
 
-    if (!deletedCategory) {
-      return next(errorHandler(401, "Categoria nu a fost găsită și nu a putut fi ștearsă."));
+    if (!category) {
+      return next(errorHandler(404, "Categoria nu a fost gasită și nu a putut fi ștearsă."));
     }
 
-    res.status(200).send("Categoria a fost ștearsă cu succes.");
+    if (category.subcategories && category.subcategories.length > 0) {
+      await Subcategory.deleteMany({ _id: { $in: category.subcategories } });
+    }
+
+    await Category.deleteOne({ _id: categoryId });
+
+    res.status(200).send("Categoria și subcategoriile sale au fost ștearse cu succes.");
   } catch (error) {
     next(errorHandler(400, "Eroare la stergerea categoriei."));
   }
