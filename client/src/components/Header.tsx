513818d4d09384dea36f864f8f52eabcc73f6f2b
Danny <varmarid@gmail.com>
2025-01-11 16:04:57 +0200

    fix delay in the header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -20,10 +20,10 @@ import {
 import { useLocation, useNavigate } from "react-router-dom";
 import { RootState } from "../redux/store";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
+  { name: "Dashboard", href: "/", current: true },
   { name: "Team", href: "#", current: false },
   { name: "Projects", href: "#", current: false },
   { name: "Calendar", href: "#", current: false },
@@ -38,22 +38,29 @@ export default function Header() {
   const navigate = useNavigate();
   const location = useLocation();
   const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated);
+  // const loading = useSelector((state: RootState) => state.user.loading);
+  const[loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
+        setLoading(true);
         const res = await axios.get("/api/auth/checkAuth");
         dispatch(signInStart());
         if (res.data.isAuthenticated) {
           dispatch(signInSuccess(res.data.user));
         }
         dispatch(signInFailure());
+
       } catch (error) {
         const errorMessage =
           axios.isAxiosError(error) && error.response
             ? error.response.data.message || "A apărut o eroare la autentificare."
             : "A apărut o eroare neprevăzută.";
         dispatch(signInFailure(errorMessage));
+      }
+      finally {
+        setLoading(false);
       }
     };
 
@@ -128,84 +135,90 @@ export default function Header() {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {isAuthenticated ? (
-              <>
-              {location.pathname !== "/item" && ( 
-                <button
-                  type="button"
-                  className="relative rounded-full bg-white p-1 text-gray-900 hover:text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  onClick={() => navigate("/item")}
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Add Item</span>
-                  <PlusIcon aria-hidden="true" className="size-6" />
-                </button>
-                )}
-
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        alt=""
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        className="size-8 rounded-full"
-                      />
-                    </MenuButton>
-                  </div>
-                  <MenuItems
-                    transition
-                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                  >
-                    <MenuItem>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-                      >
-                        Your Profile
-                      </a>
-                    </MenuItem>
-                    <MenuItem>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-                      >
-                        Settings
-                      </a>
-                    </MenuItem>
-                    <MenuItem>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-                        onClick={handleSignOut}
-                      >
-                        Sign out
-                      </a>
-                    </MenuItem>
-                  </MenuItems>
-                </Menu>
-              </>
+            {loading ? (
+              <></>
             ) : (
               <>
-                <span className="hidden md:block">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100"
-                    onClick={handleSignUp}
-                  >
-                    Sign Up
-                  </button>
-                </span>
-                <span className="">
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-800"
-                    onClick={handleSignIn}
-                  >
-                    Sign In
-                  </button>
-                </span>
+                {isAuthenticated ? (
+                  <>
+                    {location.pathname !== "/item" && (
+                      <button
+                        type="button"
+                        className="relative rounded-full bg-white p-1 text-gray-900 hover:text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        onClick={() => navigate("/item")}
+                      >
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Add Item</span>
+                        <PlusIcon aria-hidden="true" className="size-6" />
+                      </button>
+                    )}
+
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                          <span className="absolute -inset-1.5" />
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            alt=""
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            className="size-8 rounded-full"
+                          />
+                        </MenuButton>
+                      </div>
+                      <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                      >
+                        <MenuItem>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                          >
+                            Your Profile
+                          </a>
+                        </MenuItem>
+                        <MenuItem>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                          >
+                            Settings
+                          </a>
+                        </MenuItem>
+                        <MenuItem>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                            onClick={handleSignOut}
+                          >
+                            Sign out
+                          </a>
+                        </MenuItem>
+                      </MenuItems>
+                    </Menu>
+                  </>
+                ) : (
+                  <>
+                    <span className="hidden md:block">
+                      <button
+                        type="button"
+                        className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100"
+                        onClick={handleSignUp}
+                      >
+                        Sign Up
+                      </button>
+                    </span>
+                    <span className="">
+                      <button
+                        type="button"
+                        className="inline-flex items-center rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-800"
+                        onClick={handleSignIn}
+                      >
+                        Sign In
+                      </button>
+                    </span>
+                  </>
+                )}
               </>
             )}
           </div>
